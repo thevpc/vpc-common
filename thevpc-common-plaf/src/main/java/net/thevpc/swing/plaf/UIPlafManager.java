17bccd8bf7da4c82b37df54fad344e83ed309966
thevpc <taha.bensalah@gmail.com>
2021-12-19 12:10:31 +0100

    [CHANGED] add support to simple names "metal","cde","motif" and "nimbus"

--- thevpc-common-plaf/src/main/java/net/thevpc/swing/plaf/UIPlafManager.java
+++ thevpc-common-plaf/src/main/java/net/thevpc/swing/plaf/UIPlafManager.java
@@ -5,27 +5,19 @@
  */
 package net.thevpc.swing.plaf;
 
-import com.formdev.flatlaf.FlatDarculaLaf;
-import com.formdev.flatlaf.FlatDarkLaf;
-import com.formdev.flatlaf.FlatIntelliJLaf;
-import com.formdev.flatlaf.FlatLightLaf;
-import com.formdev.flatlaf.IntelliJTheme;
-import java.awt.Font;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Enumeration;
-import java.util.LinkedHashMap;
-import java.util.List;
-import javax.swing.UIDefaults;
-import javax.swing.UIManager;
+import com.formdev.flatlaf.*;
+
+import javax.swing.*;
 import javax.swing.plaf.FontUIResource;
 import javax.swing.plaf.metal.MetalLookAndFeel;
 import javax.swing.plaf.metal.OceanTheme;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.List;
+import java.util.*;
 
 /**
- *
  * @author thevpc
  */
 public class UIPlafManager {
@@ -39,13 +31,13 @@ public class UIPlafManager {
 
     public UIPlafManager() {
         UIManager.addPropertyChangeListener(new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent evt) {
-                if ("lookAndFeel".equals(evt.getPropertyName())) {
-                    onPreparePlaf();
-                }
-            }
-        }
+                                                @Override
+                                                public void propertyChange(PropertyChangeEvent evt) {
+                                                    if ("lookAndFeel".equals(evt.getPropertyName())) {
+                                                        onPreparePlaf();
+                                                    }
+                                                }
+                                            }
         );
         for (UIManager.LookAndFeelInfo p : UIManager.getInstalledLookAndFeels()) {
             switch (p.getName()) {
@@ -123,23 +115,23 @@ public class UIPlafManager {
         });
 
         for (String lightTheme : new String[]{
-            "Gray",
-            "GitHub_Contrast",
-            "GitHub",
-            "Arc_Theme_Orange",
-            "Arc_Theme",
-            "Cyan",
-            "Atom_One_Light_Contrast",
-            "Atom_One_Light",
-            "LightFlatTheme",
-            "Light_Owl_Contrast",
-            "Light_Owl",
-            "Light",
-            "Material_Lighter_Contrast",
-            "Material_Lighter",
-            "Solarized_Light_Contrast",
-            "Solarized_Light",
-            "Solarized_Light_Theme"
+                "Gray",
+                "GitHub_Contrast",
+                "GitHub",
+                "Arc_Theme_Orange",
+                "Arc_Theme",
+                "Cyan",
+                "Atom_One_Light_Contrast",
+                "Atom_One_Light",
+                "LightFlatTheme",
+                "Light_Owl_Contrast",
+                "Light_Owl",
+                "Light",
+                "Material_Lighter_Contrast",
+                "Material_Lighter",
+                "Solarized_Light_Contrast",
+                "Solarized_Light",
+                "Solarized_Light_Theme"
         }) {
             boolean dark = false;
             boolean light = true;
@@ -149,46 +141,46 @@ public class UIPlafManager {
                 @Override
                 public void apply() throws Exception {
                     IntelliJTheme.install(ClassLoader.getSystemClassLoader().getResourceAsStream(
-                            "net/thevpc/swings/plaf/intellijthemes/" + lightTheme + ".theme.json"
-                    )
+                                    "net/thevpc/swings/plaf/intellijthemes/" + lightTheme + ".theme.json"
+                            )
                     );
                 }
             });
         }
 
         for (String theme : new String[]{
-            "MaterialTheme",
-            "HighContrast",
-            "Material_Oceanic_Contrast",
-            "Material_Oceanic",
-            "Material_Palenight_Contrast",
-            "Material_Palenight",
-            "Monocai",
-            "Monokai_Pro_Contrast",
-            "Monokai_Pro",
-            "Spacegray",
-            "Gruvbox_Theme",
-            "Hiberbee",
-            "DarkFlatTheme",
-            "DarkPurple",
-            "Arc_Dark",
-            "Arc_Dark_Contrast",
-            "Atom_One_Dark_Contrast",
-            "Atom_One_Dark",
-            "Dracula_Contrast",
-            "Dracula",
-            "Dracula2",
-            "Material_Darker_Contrast",
-            "Material_Darker",
-            "Material_Deep_Ocean_Contrast",
-            "Material_Deep_Ocean",
-            "Night_Owl_Contrast",
-            "Night_Owl",
-            "Nord",
-            "One_Dark",
-            "Solarized_Dark_Contrast",
-            "Solarized_Dark",
-            "solarized_Dark_Theme",}) {
+                "MaterialTheme",
+                "HighContrast",
+                "Material_Oceanic_Contrast",
+                "Material_Oceanic",
+                "Material_Palenight_Contrast",
+                "Material_Palenight",
+                "Monocai",
+                "Monokai_Pro_Contrast",
+                "Monokai_Pro",
+                "Spacegray",
+                "Gruvbox_Theme",
+                "Hiberbee",
+                "DarkFlatTheme",
+                "DarkPurple",
+                "Arc_Dark",
+                "Arc_Dark_Contrast",
+                "Atom_One_Dark_Contrast",
+                "Atom_One_Dark",
+                "Dracula_Contrast",
+                "Dracula",
+                "Dracula2",
+                "Material_Darker_Contrast",
+                "Material_Darker",
+                "Material_Deep_Ocean_Contrast",
+                "Material_Deep_Ocean",
+                "Night_Owl_Contrast",
+                "Night_Owl",
+                "Nord",
+                "One_Dark",
+                "Solarized_Dark_Contrast",
+                "Solarized_Dark",
+                "solarized_Dark_Theme",}) {
             boolean dark = true;
             boolean light = false;
             boolean contrast = theme.toLowerCase().contains("contrast");
@@ -197,114 +189,17 @@ public class UIPlafManager {
                 @Override
                 public void apply() throws Exception {
                     IntelliJTheme.install(ClassLoader.getSystemClassLoader().getResourceAsStream(
-                            "net/thevpc/swings/plaf/intellijthemes/" + theme + ".theme.json"
-                    )
+                                    "net/thevpc/swings/plaf/intellijthemes/" + theme + ".theme.json"
+                            )
                     );
                 }
             });
         }
     }
 
-    protected String nameFromId(String a) {
-        StringBuilder sb = new StringBuilder();
-        char[] charArray = a.toCharArray();
-        for (int i = 0; i < charArray.length; i++) {
-            char c = charArray[i];
-            if (c == '_' || c == '-') {
-                sb.append(' ');
-            } else if (i > 0 && i < charArray.length - 1 && Character.isLowerCase(charArray[i - 1]) && Character.isUpperCase(charArray[i])) {
-                sb.append(' ');
-                sb.append(c);
-            } else {
-                sb.append(c);
-            }
-        }
-        return sb.toString();
-    }
-
-    public final UIPlaf get(String id) {
-        return installed.get(id);
-    }
-
-    public final List<UIPlaf> items() {
-        ArrayList<UIPlaf> s = new ArrayList<>(installed.values());
-        s.sort(new Comparator<UIPlaf>() {
-            @Override
-            public int compare(UIPlaf o1, UIPlaf o2) {
-                return o1.getName().compareTo(o2.getName());
-            }
-        });
-        return s;
-    }
-
-    public final void addListener(UIPlafListener listener) {
-        listeners.add(listener);
-    }
-
     public final static UIPlafManager getCurrentManager() {
         UIPlafManager a = (UIPlafManager) UIManager.get(UIPlafManager.class.getName());
         return a != null ? a : INSTANCE;
-    }
-
-    public final UIPlaf getCurrent() {
-        return current;
-    }
-
-    public final void apply(String name) {
-        apply(installed.get(name));
-    }
-
-    public final void apply(UIPlaf a) {
-        if (a != null) {
-            UIPlaf old = current;
-            current = a;
-            try {
-                a.apply();
-            } catch (Exception ex) {
-                throw new IllegalArgumentException(ex);
-            }
-            UIManager.put(UIPlafManager.class.getName() + ".plaf", a);
-            UIManager.put(UIPlafManager.class.getName() + ".dark", a.isDark());
-            UIManager.put(UIPlafManager.class.getName() + ".contrast", a.isContrast());
-            UIManager.put(UIPlafManager.class.getName() + ".system", a.isSystem());
-            UIManager.put(UIPlafManager.class.getName() + ".light", a.isLight());
-            UIManager.put(UIPlafManager.class.getName() + ".name", a.getName());
-            for (UIPlafListener listener : listeners) {
-                listener.plafChanged(current);
-            }
-        }
-    }
-
-    public boolean resizeRelativeFonts(float f) {
-        if (f > 0 && f < 4) {
-            this.appFontRelative = Float.NaN;
-            this.appFontAbsolute = f;
-            apply(current);
-            return true;
-        }
-        return false;
-    }
-
-    public boolean resizeAbsoluteFonts(float f) {
-        if (f > 0 && f <= 128) {
-            this.appFontRelative = Float.NaN;
-            this.appFontAbsolute = f;
-            apply(current);
-            return true;
-        }
-        return false;
-    }
-
-    private void onPreparePlaf() {
-        if (!Float.isNaN(appFontRelative)) {
-            resizeAllUIFontsRelative(appFontRelative);
-        } else if (!Float.isNaN(appFontAbsolute)) {
-            resizeAllUIFontsAbsolute(appFontAbsolute);
-        }
-    }
-
-    public final void add(UIPlaf a) {
-        installed.put(a.getId(), a);
     }
 
     private static void resizeAllUIFontsAbsolute(float absolute) {
@@ -361,6 +256,161 @@ public class UIPlafManager {
         }
     }
 
+    protected String nameFromId(String a) {
+        StringBuilder sb = new StringBuilder();
+        char[] charArray = a.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            if (c == '_' || c == '-') {
+                sb.append(' ');
+            } else if (i > 0 && i < charArray.length - 1 && Character.isLowerCase(charArray[i - 1]) && Character.isUpperCase(charArray[i])) {
+                sb.append(' ');
+                sb.append(c);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public final UIPlaf get(String id) {
+        return installed.get(id);
+    }
+
+    public final List<UIPlaf> items() {
+        ArrayList<UIPlaf> s = new ArrayList<>(installed.values());
+        s.sort(new Comparator<UIPlaf>() {
+            @Override
+            public int compare(UIPlaf o1, UIPlaf o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        return s;
+    }
+
+    public final void addListener(UIPlafListener listener) {
+        listeners.add(listener);
+    }
+
+    public final UIPlaf getCurrent() {
+        return current;
+    }
+
+    public final void apply(String name) {
+        if (name != null) {
+            UIPlaf a = installed.get(name);
+            if (a != null) {
+                apply(a);
+            } else {
+                switch (name.trim().toLowerCase()) {
+                    case "metal": {
+                        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                            if (info.getClassName().equals(UIManager.getCrossPlatformLookAndFeelClassName())) {
+                                a = installed.get(info.getName());
+                                if (a != null) {
+                                    apply(a);
+                                    return;
+                                }
+                            }
+                        }
+                        break;
+                    }
+                    case "gtk": {
+                        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                            if ("GTK+".equals(info.getName())) {
+                                a = installed.get(info.getName());
+                                if (a != null) {
+                                    apply(a);
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    case "cde":
+                    case "motif": {
+                        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                            if ("CDE/Motif".equals(info.getName())) {
+                                a = installed.get(info.getName());
+                                if (a != null) {
+                                    apply(a);
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    case "nimbus": {
+                        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                            if ("Nimbus".equals(info.getName())) {
+                                a = installed.get(info.getName());
+                                if (a != null) {
+                                    apply(a);
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public final void apply(UIPlaf a) {
+        if (a != null) {
+            UIPlaf old = current;
+            current = a;
+            try {
+                a.apply();
+            } catch (Exception ex) {
+                throw new IllegalArgumentException(ex);
+            }
+            UIManager.put(UIPlafManager.class.getName() + ".plaf", a);
+            UIManager.put(UIPlafManager.class.getName() + ".dark", a.isDark());
+            UIManager.put(UIPlafManager.class.getName() + ".contrast", a.isContrast());
+            UIManager.put(UIPlafManager.class.getName() + ".system", a.isSystem());
+            UIManager.put(UIPlafManager.class.getName() + ".light", a.isLight());
+            UIManager.put(UIPlafManager.class.getName() + ".name", a.getName());
+            for (UIPlafListener listener : listeners) {
+                listener.plafChanged(current);
+            }
+        }
+    }
+
+    public boolean resizeRelativeFonts(float f) {
+        if (f > 0 && f < 4) {
+            this.appFontRelative = Float.NaN;
+            this.appFontAbsolute = f;
+            apply(current);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean resizeAbsoluteFonts(float f) {
+        if (f > 0 && f <= 128) {
+            this.appFontRelative = Float.NaN;
+            this.appFontAbsolute = f;
+            apply(current);
+            return true;
+        }
+        return false;
+    }
+
+    private void onPreparePlaf() {
+        if (!Float.isNaN(appFontRelative)) {
+            resizeAllUIFontsRelative(appFontRelative);
+        } else if (!Float.isNaN(appFontAbsolute)) {
+            resizeAllUIFontsAbsolute(appFontAbsolute);
+        }
+    }
+
+    public final void add(UIPlaf a) {
+        installed.put(a.getId(), a);
+    }
+
+    public float getAppFontRelative() {
+        return appFontRelative;
+    }
+
     private static class Font2 extends Font {
 
         int initialSize;
@@ -375,10 +425,6 @@ public class UIPlafManager {
             this.initialSize = initialSize;
         }
 
-    }
-
-    public float getAppFontRelative() {
-        return appFontRelative;
     }
 
 }
